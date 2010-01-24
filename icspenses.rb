0fb49df9bc2b364f69ebb79af3a0bf4430f6e2a2
Henrik N <henrik@nyh.se>
2010-01-24 16:50:37 +0100

    Move top-level scraping from ICABanken to ICABanken::Customer.

--- icspenses.rb
+++ icspenses.rb
@@ -1,11 +1,16 @@
 # ICspenses.
 # Parse ICA-banken transactions. To be used for expense tracking.
 # By Henrik Nyh <http://henrik.nyh.se> 2010-01-24 under the MIT license.
+#
+# Under development. Provide personnummer and PIN as command-line arguments:
+#
+#   ruby icspenses.rb 750123-4567 1234
 
 require "rubygems"
 require "mechanize"
 
 class ICABanken
+
 
   class LoginError < StandardError
     attr_reader :code, :html
@@ -23,13 +28,19 @@ class ICABanken
     end
   end
   
+
   class Transaction < Struct.new(:date, :amount, :details, :autogiro)
     def to_s
       "#{date}: #{amount}#{"*" if autogiro} (#{details})"
     end
   end
   
+
   class Account < Struct.new(:agent, :id, :number, :name)
+    
+    def to_s
+      "#{number} (#{name})"
+    end
     
     def statement(from, unto)
       page = self.agent.get(statement_url(from, unto))
@@ -60,66 +71,69 @@ class ICABanken
     
   end
   
-
-  def initialize(pnr, pwd)
-    @pnr = pnr
-    @pwd = pwd
-    @agent = WWW::Mechanize.new
-  end
   
-  def login
-    @page = @agent.get(login_url)
-
-    check_for_errors!
-    submit_login_form
-    discover_accounts
-    
-  rescue DoubleSessionError
-    may_retry = !@has_retried
-    @has_retried = true
-    may_retry ? retry : raise
-  rescue LoginError => e
-    puts e.message
-  end
+  class Customer
   
-  def accounts
-    @accounts
-  end
-  
-protected
-
-  def login_url
-    "https://www.icabanken.se/Secure/Login/LoginPw.aspx?JSEnabled=1&Pnr=#{@pnr}"
-  end
-  
-  def check_for_errors!
-    error_code_field = @page.at('#lastErrCode')
-    error_code = error_code_field && error_code_field['value'].to_i.nonzero?
-    
-    if error_code == DoubleSessionError::CODE
-      raise DoubleSessionError.new
-    elsif error_code
-      raise LoginError.new(error_code, @page.at('title').text.strip)
+    def initialize(pnr, pwd)
+      @pnr = pnr
+      @pwd = pwd
+      @agent = WWW::Mechanize.new
     end
-  end
   
-  def submit_login_form
-    form = WWW::Mechanize::Form.new(@page.at('.login-simple'), @agent, @page)
-    form.JSEnabled = "1"
-    form.Password = @pwd
-    
-    @page = form.submit
-  end
-  
-  def discover_accounts
-    @accounts = @page.links_with(:href => /AccountId=/).map do |link|
-      tr = link.node.parent.parent
+    def login
+      @page = @agent.get(login_url)
 
-      id     = link.href[/AccountId=(\d+)/, 1]
-      number = link.text
-      name   = tr.css('td')[1].text
+      check_for_errors!
+      submit_login_form
+      discover_accounts
+    
+    rescue DoubleSessionError
+      may_retry = !@has_retried
+      @has_retried = true
+      may_retry ? retry : raise
+    rescue LoginError => e
+      puts e.message
+    end
+  
+    def accounts
+      @accounts
+    end
+  
+  protected
+
+    def login_url
+      "https://www.icabanken.se/Secure/Login/LoginPw.aspx?JSEnabled=1&Pnr=#{@pnr}"
+    end
+  
+    def check_for_errors!
+      error_code_field = @page.at('#lastErrCode')
+      error_code = error_code_field && error_code_field['value'].to_i.nonzero?
+    
+      if error_code == DoubleSessionError::CODE
+        raise DoubleSessionError.new
+      elsif error_code
+        raise LoginError.new(error_code, @page.at('title').text.strip)
+      end
+    end
+  
+    def submit_login_form
+      form = WWW::Mechanize::Form.new(@page.at('.login-simple'), @agent, @page)
+      form.JSEnabled = "1"
+      form.Password = @pwd
+    
+      @page = form.submit
+    end
+  
+    def discover_accounts
+      @accounts = @page.links_with(:href => /AccountId=/).map do |link|
+        tr = link.node.parent.parent
+
+        id     = link.href[/AccountId=(\d+)/, 1]
+        number = link.text
+        name   = tr.css('td')[1].text
       
-      Account.new(@agent, id, number, name)
+        Account.new(@agent, id, number, name)
+      end
     end
   end
   
@@ -131,15 +145,17 @@ if $0 == __FILE__
   pnr = ARGV[0]
   pwd = ARGV[1]
 
-  ica = ICABanken.new(pnr, pwd)
-  ica.login
+  customer = ICABanken::Customer.new(pnr, pwd)
+  customer.login
   
-  account = ica.accounts.find {|a| a.name == "Betalkort" }
+  puts "Logged in as customer #{pnr}."
+  puts
+  
+  account = customer.accounts.find {|a| a.name == "Betalkort" }
 
+  puts "Account #{account}"
   puts
-  puts "Account #{account.number} (#{account.name})"
-  puts
-  transactions = account.statement(Date.new(2009,12,23), Date.today)
+  transactions = account.statement(Date.new(2010,1,1), Date.today)
   transactions.each do |transaction|
     puts transaction
   end
